a3a598fad0da29c1ca1cbd1552f71f5b182d31cb
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-05 23:23:20 +0200

    adjusts syntax.cpp

--- c++/syntax.cpp
+++ c++/syntax.cpp
@@ -4,8 +4,11 @@
 using namespace std; // allows us to use the standard library without prefixing it with std::
 
 int main() { // is a function, code inside {} is executed
-    std::cout << "Hello World!"; // cout "see-out" is used to print to the console, << is the insertion operator to output/print the text
+    std::cout << "Hello World!\n"; // cout "see-out" is used to print to the console, << is the insertion operator to output/print the text
     // care: cout and Cout are different. every statement ends with ; 
+    cout << "Hi there!\n";
+    cout << "Have a great day!\n"; 
     return 0; // end main function
 }
 
+// \n forces a new line
