2419e77260015a8ce98a1bace1b69e6a0e6be6c1
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-05 23:04:50 +0200

    finishes hello-syntax

--- c++/hello-syntax.cpp
+++ c++/hello-syntax.cpp
@@ -1,1 +1,11 @@
 /* this file will explain the syntax used in hello.cpp */
+
+# include <iostream> // is a header file library that has input and output functions, i.e. cout and cin -> adds functionality
+using namespace std; // allows us to use the standard library without prefixing it with std::
+
+int main() { // is a function, code inside {} is executed
+    std::cout << "Hello World!"; // cout "see-out" is used to print to the console, << is the insertion operator to output/print the text
+    // care: cout and Cout are different. every statement ends with ; 
+    return 0; // end main function
+}
+
