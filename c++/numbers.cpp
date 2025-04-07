0c14528fd6b0fe085d2a1759cf78c7fd0e8aa6ce
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-07 17:36:06 +0200

    some calculations and more syntax

---
+++ c++/numbers.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << 3 << "\n"; // \n must be a string, otherwise endl 
+    cout << 3+3 << "\n\n"; // \n\n creates empty line
+    cout << 2 * 5 << endl;
+    cout << "\"These are calculations.\" \t This creates space. \\"; // \t is tab and \\ is backslash "\ is double quote
+    return 0;
+}
+
+
