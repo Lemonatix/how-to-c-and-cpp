e503e8b827d66c215e3247641dd67aed338d9616
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-07 18:26:49 +0200

    adds more code

--- c++/variables.cpp
+++ c++/variables.cpp
@@ -11,3 +11,26 @@ int main() {
   cout << myNum2;
   return 0;
 }
+
+int main1 () {
+  int myNum = 5; // Integer (whole number)
+  float myFloatNum = 5.99; // Floating point number
+  char myLetter = 'D'; // Character
+  string myText = "Hello"; // String
+  bool myBoolean = true; // Boolean (true or false)
+
+  cout << myNum << "\n";
+  cout << myFloatNum << "\n";
+  cout << myLetter << "\n";
+  cout << myText << "\n";
+  cout << myBoolean;
+}
+
+int main2 () {
+    int myAge = 21;
+    cout << "I am   " << myAge << " years old.";
+    int x = 7;
+    int y = 3;
+    int sum = x + y;
+    cout << sum;
+}
