eae91bbadd2b86cef13918f8c5c7d314bf16f330
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-07 18:35:12 +0200

    fixes mssing output of main1/2

--- c++/variables.cpp
+++ c++/variables.cpp
@@ -4,39 +4,50 @@
 #include <string>
 using namespace std;
 
+// Function declarations
 int main1();
 int main2();
 
 int main() {
-  int myNum;
-  int myNum2 = 5;
-  myNum = 15;
-  cout << myNum << "\n";
-  cout << myNum2;
-  return 0;
+    int myNum;
+    int myNum2 = 5;
+    myNum = 15;
+    cout << myNum << "\n";
+    cout << myNum2 << "\n";
+
+    cout << "\n--- main1 output ---\n";
+    main1();
+
+    cout << "\n--- main2 output ---\n";
+    main2();
+
+    return 0;
 }
 
-int main1 () {
-  int myNum = 5; // Integer (whole number)
-  float myFloatNum = 5.99; // Floating point number
-  char myLetter = 'D'; // Character
-  string myText = "Hello"; // String
-  bool myBoolean = true; // Boolean (true or false)
+int main1() {
+    int myNum = 5;               // Integer (whole number)
+    float myFloatNum = 5.99;     // Floating point number
+    char myLetter = 'D';         // Character
+    string myText = "Hello";     // String
+    bool myBoolean = true;       // Boolean (true or false)
 
-  cout << myNum << "\n";
-  cout << myFloatNum << "\n";
-  cout << myLetter << "\n";
-  cout << myText << "\n";
-  cout << myBoolean;
-  return 0;
+    cout << myNum << "\n";
+    cout << myFloatNum << "\n";
+    cout << myLetter << "\n";
+    cout << myText << "\n";
+    cout << myBoolean << "\n";
+
+    return 0;
 }
 
-int main2 () {
+int main2() {
     int myAge = 21;
-    cout << "I am   " << myAge << " years old.";
+    cout << "I am   " << myAge << " years old.\n";
+
     int x = 7;
     int y = 3;
     int sum = x + y;
-    cout << sum;
+    cout << "Sum: " << sum << "\n";
+
     return 0;
 }
