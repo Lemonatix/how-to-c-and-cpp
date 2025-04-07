66ac422f1238aeaad75eadd97cd20e024d134af6
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-07 18:22:44 +0200

    adds some code

---
+++ c++/variables.cpp
@@ -0,0 +1,13 @@
+// syntax: type variableName = value;
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum;
+  int nyNum2 = 5;
+  myNum = 15;
+  cout << myNum << "\n";
+  cout << nyNum2;
+  return 0;
+}
