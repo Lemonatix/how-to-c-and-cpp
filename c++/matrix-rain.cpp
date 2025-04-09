dd6e26bfe7f7bb4edbb9148ab5acedd47c5a2972
Lemonatix <ph.mika.ma.riesterer@gmail.com>
2025-04-09 19:27:31 +0200

    adds code

--- c++/matrix-rain.cpp
+++ c++/matrix-rain.cpp
@@ -6,3 +6,24 @@
 const int WIDTH = 80;
 const int HEIGHT = 24;
 
+int main() {
+    srand(time(0)); // Seed the random number generator
+
+    while(true) {
+        for (int i = 0; i < HEIGHT; ++i) {
+            for (int j = 0; j < WIDTH; ++j) {
+                int r = rand() % 100; // Random number between 0 and 99
+                if (r < 10)
+                    std::cout << "0";
+                else if (r < 20)
+                    std::cout << "1";
+                else 
+                    std::cout << " "; // Space for other numbers
+            }
+            std::cout << std::endl;
+        }
+        usleep(100000); 
+        std::cout << "\033[2J\033[1;1H";
+    }
+    return 0;
+}
